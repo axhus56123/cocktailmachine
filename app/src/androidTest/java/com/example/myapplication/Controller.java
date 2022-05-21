eb5f2de2b563de7f8624e7e61bf79868c758550d
沈啟文 <you@example.comkevin89722939@gmail.com>
2022-05-21 22:30:09 +0800

    init

---
+++ app/src/androidTest/java/com/example/myapplication/Controller.java
@@ -0,0 +1,15 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Controller extends Activity {
+        public void buttonOnClick(View view) {
+        }
+}
