ef5c42a6c99fcbdee1d5fd91ec4fed91672887b2
沈啟文 <you@example.comkevin89722939@gmail.com>
2022-10-25 21:22:37 +0800

    send to firebase

--- app/src/main/java/com/example/wifitest/Account.java
+++ app/src/main/java/com/example/wifitest/Account.java
@@ -1,5 +1,6 @@
 package com.example.wifitest;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,14 +10,20 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class Account extends AppCompatActivity {
 
-    TextView tvEmail;
+    TextView tvEmail,tvname,tvid;
+    FirebaseAuth mAuth;
+    FirebaseFirestore db;
     Button logout;
     ImageButton back;
 
@@ -27,6 +34,26 @@ public class Account extends AppCompatActivity {
         FirebaseAuth auth = FirebaseAuth.getInstance();
         FirebaseUser currentUser = auth.getCurrentUser();
         back = findViewById(R.id.drinkBack);
+        tvname = findViewById(R.id.tvname);
+        tvid = findViewById(R.id.tvid);
+        mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+        FirebaseUser user = mAuth.getCurrentUser();
+        String uid = user.getUid();
+        db.collection("user").document(uid).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if(task.isSuccessful()){
+                    DocumentSnapshot documentSnapshot = task.getResult();
+                    if(documentSnapshot.exists()){
+                        String name = documentSnapshot.get("name",String.class);
+                        String id = documentSnapshot.get("id",String.class);
+                        tvname.setText("Name:"+name);
+                        tvid.setText("Id:"+id);
+                    }
+                }
+            }
+        });
 
         if(currentUser == null){
             Intent intent = new Intent(this, Login.class);
--- app/src/main/java/com/example/wifitest/Register.java
+++ app/src/main/java/com/example/wifitest/Register.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,13 +17,18 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Random;
 
 public class Register extends AppCompatActivity {
 
     Activity context = this;
     FirebaseAuth mAuth;
-    EditText etacc,etpass;
-    TextView tv4,login;
+    FirebaseFirestore db;
+    EditText etacc,etpass,etname;
+    TextView tv8,login;
     Button sure;
 
     @Override
@@ -31,22 +37,46 @@ public class Register extends AppCompatActivity {
         setContentView(R.layout.activity_register);
         etacc = findViewById(R.id.etRegisterEmail);
         etpass = findViewById(R.id.etRegisterpass);
-        tv4 = findViewById(R.id.tv8);
+        tv8 = findViewById(R.id.tv8);
         sure = findViewById(R.id.btnlogout);
         login = findViewById(R.id.textViewlogin);
         mAuth = FirebaseAuth.getInstance();
+        etname = findViewById(R.id.etRegistername);
+
+        db = FirebaseFirestore.getInstance();
 
         sure.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String email = etacc.getText().toString().trim();
+                String password = etpass.getText().toString().trim();
+                String name = etname.getText().toString().trim();
+                if( TextUtils.isEmpty(email)||TextUtils.isEmpty(password)||TextUtils.isEmpty(name)){
+                    tv8.setText("請輸入數值");
+                    return;
+                }
+
                 mAuth.createUserWithEmailAndPassword(etacc.getText().toString(),etpass.getText().toString()).addOnCompleteListener(context, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(task.isSuccessful()){
                             FirebaseUser user=mAuth.getCurrentUser();
-                            tv4.setText("結果:"+user.getEmail()+"註冊成功");
+                            String uid = user.getUid();
+                            HashMap<String,Object> datauser = new HashMap<>();
+                            datauser.put("name",name);
+                            datauser.put("id",getRandom(6));
+
+                            db.collection("user").document(uid).set(datauser).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+
+                                }
+                            });
+
+
+                            tv8.setText("結果:"+user.getEmail()+"註冊成功");
                         }else{
-                            tv4.setText("結果:註冊失敗"+task.getException());
+                            tv8.setText("結果:註冊失敗"+task.getException());
                         }
                     }
                 });
@@ -67,5 +97,15 @@ public class Register extends AppCompatActivity {
         Intent intent = new Intent(this, Login.class);
         startActivity(intent);
         finish();
+    }
+    private String getRandom(int i){
+        String characters = "abcdefghijklmnopqrstuvwxyz1234567890";
+        StringBuilder result = new StringBuilder();
+        while (i>0){
+            Random random = new Random();
+            result.append(characters.charAt(random.nextInt(characters.length())));
+            i--;
+        }
+        return  result.toString();
     }
 }
